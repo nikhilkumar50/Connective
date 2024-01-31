7fd949bcd7fb278d9d08ab75d7915e24b63d2728
nikhilkumar50 <nikhilkumarray9889@gmail.com>
2024-01-31 17:12:05 +0530

    chat integration

--- frontend/src/atoms/commentsAtom.js
+++
@@ -1,7 +0,0 @@
-import {atom} from "recoil";
-
-const commentsAtom=atom({
-    key:"commentsAtom",
-    default:[],
-});
-export default commentsAtom;
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -34,100 +34,100 @@ const ChatPage = () => {
   const showToast = useShowToast();
   const { socket, onlineUsers } = useSocket();
 
-  useEffect(() => {
-    socket?.on("messagesSeen", ({ conversationId }) => {
-      setConversations((prev) => {
-        const updatedConversations = prev.map((conversation) => {
-          if (conversation._id === conversationId) {
-            return {
-              ...conversation,
-              lastMessage: {
-                ...conversation.lastMessage,
-                seen: true,
-              },
-            };
-          }
-          return conversation;
-        });
-        return updatedConversations;
-      });
-    });
-  }, [socket, setConversations]);
+  // useEffect(() => {
+  //   socket?.on("messagesSeen", ({ conversationId }) => {
+  //     setConversations((prev) => {
+  //       const updatedConversations = prev.map((conversation) => {
+  //         if (conversation._id === conversationId) {
+  //           return {
+  //             ...conversation,
+  //             lastMessage: {
+  //               ...conversation.lastMessage,
+  //               seen: true,
+  //             },
+  //           };
+  //         }
+  //         return conversation;
+  //       });
+  //       return updatedConversations;
+  //     });
+  //   });
+  // }, [socket, setConversations]);
 
-  useEffect(() => {
-    const getConversations = async () => {
-      try {
-        const res = await fetch("/api/messages/conversations");
-        const data = await res.json();
-        if (data.error) {
-          showToast("Error", data.error, "error");
-          return;
-        }
-        console.log(data);
-        setConversations(data);
-      } catch (error) {
-        showToast("Error", error.message, "error");
-      } finally {
-        setLoadingConversations(false);
-      }
-    };
+  // useEffect(() => {
+  //   const getConversations = async () => {
+  //     try {
+  //       const res = await fetch("/api/messages/conversations");
+  //       const data = await res.json();
+  //       if (data.error) {
+  //         showToast("Error", data.error, "error");
+  //         return;
+  //       }
+  //       console.log(data);
+  //       setConversations(data);
+  //     } catch (error) {
+  //       showToast("Error", error.message, "error");
+  //     } finally {
+  //       setLoadingConversations(false);
+  //     }
+  //   };
 
-    getConversations();
-  }, [showToast, setConversations]);
+  //   getConversations();
+  // }, [showToast, setConversations]);
 
-  const handleConversationSearch = async (e) => {
-    e.preventDefault();
-    setSearchingUser(true);
-    try {
-      const res = await fetch(`/api/users/profile/${searchText}`);
-      const searchedUser = await res.json();
-      if (searchedUser.error) {
-        showToast("Error", searchedUser.error, "error");
-        return;
-      }
+  // const handleConversationSearch = async (e) => {
+  //   e.preventDefault();
+  //   setSearchingUser(true);
+  //   try {
+  //     const res = await fetch(`/api/users/profile/${searchText}`);
+  //     const searchedUser = await res.json();
+  //     if (searchedUser.error) {
+  //       showToast("Error", searchedUser.error, "error");
+  //       return;
+  //     }
 
-      const messagingYourself = searchedUser._id === currentUser._id;
-      if (messagingYourself) {
-        showToast("Error", "You cannot message yourself", "error");
-        return;
-      }
+  //     const messagingYourself = searchedUser._id === currentUser._id;
+  //     if (messagingYourself) {
+  //       showToast("Error", "You cannot message yourself", "error");
+  //       return;
+  //     }
 
-      const conversationAlreadyExists = conversations.find(
-        (conversation) => conversation.participants[0]._id === searchedUser._id
-      );
+  //     const conversationAlreadyExists = conversations.find(
+  //       (conversation) => conversation.participants[0]._id === searchedUser._id
+  //     );
 
-      if (conversationAlreadyExists) {
-        setSelectedConversation({
-          _id: conversationAlreadyExists._id,
-          userId: searchedUser._id,
-          username: searchedUser.username,
-          userProfilePic: searchedUser.profilePic,
-        });
-        return;
-      }
+  //     if (conversationAlreadyExists) {
+  //       setSelectedConversation({
+  //         _id: conversationAlreadyExists._id,
+  //         userId: searchedUser._id,
+  //         username: searchedUser.username,
+  //         userProfilePic: searchedUser.profilePic,
+  //       });
+  //       return;
+  //     }
 
-      const mockConversation = {
-        mock: true,
-        lastMessage: {
-          text: "",
-          sender: "",
-        },
-        _id: Date.now(),
-        participants: [
-          {
-            _id: searchedUser._id,
-            username: searchedUser.username,
-            profilePic: searchedUser.profilePic,
-          },
-        ],
-      };
-      setConversations((prevConvs) => [...prevConvs, mockConversation]);
-    } catch (error) {
-      showToast("Error", error.message, "error");
-    } finally {
-      setSearchingUser(false);
-    }
-  };
+  //     const mockConversation = {
+  //       mock: true,
+  //       lastMessage: {
+  //         text: "",
+  //         sender: "",
+  //       },
+  //       _id: Date.now(),
+  //       participants: [
+  //         {
+  //           _id: searchedUser._id,
+  //           username: searchedUser.username,
+  //           profilePic: searchedUser.profilePic,
+  //         },
+  //       ],
+  //     };
+  //     setConversations((prevConvs) => [...prevConvs, mockConversation]);
+  //   } catch (error) {
+  //     showToast("Error", error.message, "error");
+  //   } finally {
+  //     setSearchingUser(false);
+  //   }
+  // };
 
   return (
     <Box
